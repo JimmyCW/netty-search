e5dd00ea056d8caf1213d9eb9eb764c3ac5fe8ea
JCW <jimmywei19@163.com>
2019-02-11 13:02:00 +0800

    client

--- src/main/java/com/wx/nettysearch/day5/MyWebSocketServer.java
+++ src/main/java/com/wx/nettysearch/day5/MyWebSocketServer.java
@@ -8,26 +8,34 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
+import java.io.IOException;
+
 /**
  * @author weixing
  * @date 2019/2/10
  **/
 public class MyWebSocketServer {
 
-    public static void main(String[] args) throws InterruptedException {
-        EventLoopGroup boss = new NioEventLoopGroup();
-        EventLoopGroup worker = new NioEventLoopGroup();
+    public static void main(String[] args) throws InterruptedException, IOException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(boss, worker)
+            serverBootstrap.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .handler(new LoggingHandler(LogLevel.INFO))
                     .childHandler(new MyWebSocketServerInitializer());
             ChannelFuture channelFuture = serverBootstrap.bind(9005).sync();
-            channelFuture.channel().close().sync();
+//            Channel channel = channelFuture.channel();
+//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            for (;;) {
+//                channel.writeAndFlush(new TextWebSocketFrame(br.readLine() + "\r\n"));
+//            }
+            channelFuture.channel().closeFuture().sync();
         } finally {
-            boss.shutdownGracefully();
-            worker.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
         }
     }
 }
--- src/main/java/com/wx/nettysearch/day5/MyWebSocketServerHandler.java
+++ src/main/java/com/wx/nettysearch/day5/MyWebSocketServerHandler.java
@@ -3,7 +3,10 @@ package com.wx.nettysearch.day5;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 import java.time.LocalDateTime;
 
@@ -12,16 +15,19 @@ import java.time.LocalDateTime;
  * @date 2019/2/10
  **/
 public class MyWebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    private static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
         System.out.println("收到消息" + textWebSocketFrame.text());
-        Channel channel = channelHandlerContext.channel();
-        channel.writeAndFlush(new TextWebSocketFrame("服务器时间 " + LocalDateTime.now()));
+//        Channel channel = channelHandlerContext.channel();
+        channelGroup.writeAndFlush(new TextWebSocketFrame(textWebSocketFrame.text() + "  服务器时间 " + LocalDateTime.now()));
     }
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
         System.out.println("handlerAdded" + ctx.channel().id().asLongText());
+        channelGroup.add(ctx.channel());
     }
 
     @Override
